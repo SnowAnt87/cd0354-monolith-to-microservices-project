ac1642fcd8553523f0091aefc667980f096f2f6b
Anthony Steman <anthonysnow@protonmail.com>
2024-07-09 19:17:34 -0700

    Initial commit of working files

---
+++ udagram-frontend/set_env.bat
@@ -0,0 +1,15 @@
+:: This file is used for convenience of local development.
+:: DO NOT STORE YOUR CREDENTIALS INTO GIT
+
+@echo off
+set POSTGRES_USERNAME=anthony
+set POSTGRES_PASSWORD=ASmm1987!!
+set POSTGRES_HOST=database-2-awsclouddevnano.cpbnbpurzt77.us-east-1.rds.amazonaws.com
+set POSTGRES_DB=postgres
+set AWS_BUCKET=awsclouddevnano
+set AWS_REGION=us-east-1
+set AWS_PROFILE=default
+set JWT_SECRET=testing
+set URL=http://localhost:8100
+
+echo Environment variables set successfully.
